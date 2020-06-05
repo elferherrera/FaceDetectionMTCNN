d24f4262e1b60877e25647766d061e83430581d7
Fernando José Herrera Elizalde <fernandoj.herrera@softtek.com>
2020-06-05 10:35:59 -0500

    Removed CV2 from testing

--- tests/test_photo.py
+++ tests/test_photo.py
@@ -6,7 +6,6 @@ contains 18 faces. This image is used to test different
 cases while detecting a face
 """
 
-import cv2
 import numpy as np
 
 import face_detection_mtcnn
@@ -20,13 +19,26 @@ with open("tests/test_image.npy", "rb") as f:
 FACES_IN_IMAGE = 18
 
 
+def convert_color_channels(image):
+    """
+    Convert image channels from BGR to RGB
+    """
+    new_image = image.copy()
+
+    new_image[0, :, :] = image[2, :, :]
+    new_image[1, :, :] = image[1, :, :]
+    new_image[2, :, :] = image[0, :, :]
+
+    return new_image.astype("int")
+
+
 def test_number_detections():
     """
     Test to check if the correct number of detections was
     done using the detect_face function
     """
 
-    image_rgb = cv2.cvtColor(TEST_IMAGE, cv2.COLOR_BGR2RGB)
+    image_rgb = convert_color_channels(TEST_IMAGE)
     boxes, probs, faces = face_detection_mtcnn.detect_face(image_rgb)
 
     assert len(boxes) == FACES_IN_IMAGE
@@ -41,7 +53,7 @@ def test_shape_one_image():
     when only working with one image
     """
 
-    image_rgb = cv2.cvtColor(TEST_IMAGE, cv2.COLOR_BGR2RGB)
+    image_rgb = convert_color_channels(TEST_IMAGE)
     boxes, probs, faces = face_detection_mtcnn.detect_face(image_rgb)
 
     boxes_shape = np.array(boxes).shape
@@ -71,10 +83,51 @@ def test_shape_list_images():
     similar dimensions
     """
 
-    image_rgb = cv2.cvtColor(TEST_IMAGE, cv2.COLOR_BGR2RGB)
+    image_rgb = convert_color_channels(TEST_IMAGE)
 
     # Creating a list with 3 repeated images
     images = [image_rgb, image_rgb, image_rgb]
+
+    boxes, probs, faces = face_detection_mtcnn.detect_face(images)
+
+    boxes_shape = np.array(boxes).shape
+    probs_shape = np.array(probs).shape
+    faces_shape = np.array(faces).shape
+
+    # There should be 3 images with 18 faces with 4 points marking the
+    # bounding box for the face
+    assert boxes_shape == (3, 18, 4)
+
+    # There should be 3 images with 18 probs to indicate the probability
+    # of each bounding box to belong to a face
+    assert probs_shape == (3, 18)
+
+    # There should be 3 images with 18 faces with 5 points. Each point should
+    # have to values that represent the location of the 5 landmarks
+    # in the face
+    assert faces_shape == (3, 18, 5, 2)
+
+
+def test_shape_array_images():
+    """
+    Test to check the dimensions when using a numpy array of
+    images instead of a single image.
+
+    This is used with analyzing batches of images with
+    similar dimensions
+    """
+
+    image_rgb = convert_color_channels(TEST_IMAGE)
+
+    # Creating a list with 3 repeated images
+    height, width, channels = image_rgb.shape
+
+    # Using a numpy array to store the images
+    images = np.zeros((3, height, width, channels))
+
+    images[0, :, :, :] = image_rgb
+    images[1, :, :, :] = image_rgb
+    images[2, :, :, :] = image_rgb
 
     boxes, probs, faces = face_detection_mtcnn.detect_face(images)
 
